8fcd724ba893ca350af4a70ad73841d4a72bbe90
Nathan Jesudason <nathan.jesudason@gmail.com>
2020-12-05 17:35:26 -0800

    Added server-side async comm framework

--- server.js
+++ server.js
@@ -11,18 +11,47 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-//app.get('/', function(req, res, next) {
-    //var content = "<html> <body> <h1>Hello World!</h1> </body> </html>";
-    //res.status(200).send(content);
-//});
+/* app.get('/', function(req, res, next) {
+    //doesn't need to be implemented until index.html is removed
+}); */
 
 app.get('/class/:n', function(req, res, next) {
     var n = req.params.n;
     next();
 });
 
+//add a class to the list
+//expected reqBody: class name that does not exist yet
+app.post('/addClass', function (req, res, next){
+    console.log("req.body:", req.body);
+    res.status(501).send("Not yet implemented");
+});
+
+//remove a class from the list
+//expected reqBody: class name that does exist
+app.post('/removeClass', function (req, res, next){
+    console.log("req.body:", req.body);
+    res.status(501).send("Not yet implemented");
+});
+
+//add a flashcard to a class
+//request needs URI encoded class name (gotten from class URL)
+//expected reqBody: { question: q,  answer: a }
+app.post('/:class/addCard', function (req, res, next){
+    console.log("req.body:", req.body);
+    res.status(501).send("Not yet implemented");
+});
+
+//remove a flashcard from a class
+//request needs URI encoded class name (gotten from class URL)
+//expected reqBody: { question: q,  answer: a }
+app.post('/:class/removeCard', function (req, res, next){
+    console.log("req.body:", req.body);
+    res.status(501).send("Not yet implemented");
+});
+
 app.get('*', function (req, res) {
-    var content = "<html> <body> <h1>Out of my swamp!</h1> </body> </html>";
+    var content = "<html> <body> <h1>404 not found</h1> </body> </html>";
     res.status(404).send(content);
 });
 
