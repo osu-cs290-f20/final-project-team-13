f346173549c546d106e49d4b6f1c5f22f5db8507
Luna <luna@10-197-144-195.sds.oregonstate.edu>
2020-12-09 10:20:41 -0800

    Updates to js file to handle showing modals

--- public/index.js
+++ public/index.js
@@ -13,3 +13,58 @@ class_card.addEventListener('mouseover', moreOptions);
 
 var menu = document.getElementsByClass("more-options");
 menu.addEventListener('click', showMoreOptions);
+
+
+function showQuizModal() {
+	var showQuizModal = document.getElementById('quizmode-modal');
+	var modalBackground = document.getElementById('modal-background');
+  
+	showQuizModal.classList.toggle('hidden');
+	modalBackground.classList.toggle('hidden');
+}
+function showClassModal() {
+	var showClassModal = document.getElementById('class-modal');
+	var modalBackground = document.getElementById('modal-background');
+  
+	showClassModal.classList.toggle('hidden');
+	modalBackground.classList.toggle('hidden');
+}
+
+function handleModalAcceptClick() {
+	//
+}
+
+function hideQuizModal() {
+	var showQuizModal = document.getElementById('quizmode-modal');
+	var modalBackground = document.getElementById('modal-background');
+
+	showQuizModal.classList.toggle('hidden');
+	modalBackground.classList.toggle('hidden');
+}
+function hideClassModal() {
+	var showClassModal = document.getElementById('class-modal');
+	var modalBackground = document.getElementById('modal-background');
+
+	showClassModal.classList.toggle('hidden');
+	modalBackground.classList.toggle('hidden');
+}
+
+var quizmodeButton = document.getElementsByClassName('quiz-mode');
+if (quizmodeButton) {
+	quizmodeButton.addEventListener('click', showQuizModal);
+}
+var classButton = document.getElementsByClassName('add-class-button');
+if (classButton) {
+	classButton.addEventListener('click', showClassModal);
+}
+
+var modalAcceptButton = document.getElementById('modal-done');
+if (modalAcceptButton) {
+    modalAcceptButton.addEventListener('click', handleModalAcceptClick);
+}
+
+var modalHideButtons = document.getElementsByClassName('modal-hide-button');
+for (var i = 0; i < modalHideButtons.length; i++) {
+	modalHideButtons[i].addEventListener('click', hideQuizModal);
+	modalHideButtons[i].addEventListener('click', hideClassModal);
+}
