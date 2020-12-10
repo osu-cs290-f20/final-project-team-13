48dea1ea93ef2e97d8bf62a0198729feef087647
Luna <luna@10-197-144-195.sds.oregonstate.edu>
2020-12-10 00:13:45 -0800

    Hopefully  more stuff working

--- public/index.js
+++ public/index.js
@@ -151,6 +151,106 @@
 // }
 
 
+//add class
+// document.getElementById('add-class').onclick = function() {
+// 	console.log("add-class button clicked");
+// 	document.getElementById('add-class-modal').style.display = 'flex';
+// }
+
+// document.getElementById('modal-close-button').onclick = function() {
+// 	document.getElementById('add-class-modal').style.display = 'none';
+// }
+
+//quiz stuff
+var active_question = 0;
+var questions = document.getElementsByClassName('one-question');
+var next_button = document.getElementById('next-button');
+var prev_button = document.getElementById('prev-button');
+
+document.getElementById('quiz-button').onclick = function() {
+	document.getElementById('quiz').style.display = 'flex';
+	active_question = 0;
+	update_buttons();
+	update_questions();
+	console.log("quiz button clicked");
+}
+
+document.getElementById('submit').onclick = function() {
+	document.getElementById('answer').style.display = 'flex';
+}
+
+document.getElementsByClassName('quiz-mode-button').onclick = function() {
+	console.log("quiz mode button clicked");
+}
+
+document.getElementById('close-button').onclick = function() {
+	document.getElementById('quiz').style.display = 'none';
+	document.getElementById('answer-text-input').value = '';
+	document.getElementById('answer').style.display = 'none';
+}
+
+
+prev_button.onclick = function() {
+	console.log(active_question);
+	active_question -= 1;
+	update_buttons();
+	update_cards();
+}
+
+next_button.onclick = function() {
+	console.log(active_question);
+	active_question += 1;
+	update_buttons();
+	update_cards();	
+}
+
+function update_questions(){
+	for(i = 0; i < questions.length; i++){
+		if(i != active_question){
+			questions[i].style.display = 'none';
+		}
+		else{
+			questions[i].style.display = 'block';
+		}
+	}
+}
+
+function update_buttons(){
+	console.log(active_question);
+	if(questions[active_question - 1])
+		prev_button.style.display = 'block';
+	else
+		prev_button.style.display = 'none';
+
+	if(questions[active_question + 1])
+		next_button.style.display = 'block';
+	else
+		next_button.style.display = 'none';
+}
+
+var addCardButton = document.getElementById("add-new-card-button");
+if(addCardButton){
+	addCardButton.addEventListener('click', function(){
+		addFlashCard("test", "test");
+	});
+}
+
+var addClassButton = document.getElementById("add-class");
+if(addClassButton){
+	addClassButton.addEventListener('click', function(){
+		addClass("Test");
+	});
+}
+
+var flashCards = document.getElementsByClassName("each-card");
+console.log(flashCards);
+for (i = 0; i < flashCards.length; i++){
+	console.log(flashCards[i].lastElementChild);
+	flashCards[i].lastElementChild.addEventListener('click', function(){
+		removeFlashCard(event);
+	});
+}
+
 //below this is levi's flashcard shit!
 var active_card = 0;
 var flashcard_modal = document.getElementById('flashcard-modal');
