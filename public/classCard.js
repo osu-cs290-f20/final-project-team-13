3f757bf7f8ca46b00c833a02ef302ef59fd5bc96
Casey Colley <colleyc@oregonstate.edu>
2020-12-10 01:27:55 -0800

    more css work

--- public/classCard.js
+++ public/classCard.js
@@ -16,6 +16,6 @@ templates['classCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(con
     + alias4(((helper = (helper = lookupProperty(helpers,"key") || (data && lookupProperty(data,"key"))) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"key","hash":{},"data":data,"loc":{"start":{"line":2,"column":38},"end":{"line":2,"column":46}}}) : helper)))
     + "\">"
     + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":2,"column":48},"end":{"line":2,"column":56}}}) : helper)))
-    + "</a></h3>\n	\n	<button type=\"button\" class=\"more-options\" class=\"hidden\"><i class=\"fas fa-ellipsis-v\"></i></button>\n</div>\n\n<div class=\"more-options-menu\" hidden>\n	<button type=\"button\" class=\"delete-class\">Delete</button>\n</div>";
+    + "</a></h3>\r\n	\r\n	<button type=\"button\" class=\"more-options\" class=\"hidden\"><i class=\"fas fa-ellipsis-v\"></i></button>\r\n</div>\r\n\r\n<div class=\"more-options-menu\" hidden>\r\n	<button type=\"button\" class=\"delete-class\">Delete</button>\r\n</div>";
 },"useData":true});
 })();
